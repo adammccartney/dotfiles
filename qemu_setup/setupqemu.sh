1a1772cc2258c0798a4615d96015207e1317c53e
adammccartney <adam@mur.at>
2021-05-19 17:51:17 +0200

    added script to install dependencies for qemu

---
+++ qemu_setup/setupqemu.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# setupqemu.sh: installs dependencies for qemu vm manager 
+# needs to be run with sudo
+
+sudo apt-get install git libglib2.0-dev libfdt-dev libpixman-1-dev zlib1g-dev
+sudo apt-get install git-email
+sudo apt-get install libaio-dev libbluetooth-dev libbrlapi-dev libbz2-dev
+sudo apt-get install libcap-dev libcap-ng-dev libcurl4-gnutls-dev libgtk-3-dev
+sudo apt-get install libibverbs-dev libjpeg8-dev libncurses5-dev libnuma-dev
+sudo apt-get install librbd-dev librdmacm-dev
+sudo apt-get install libsasl2-dev libsdl1.2-dev libseccomp-dev libsnappy-dev libssh2-1-dev
+sudo apt-get install libvde-dev libvdeplug-dev libvte-2.90-dev libxen-dev liblzo2-dev
+sudo apt-get install valgrind xfslibs-dev 
+sudo apt-get install libnfs-dev libiscsi-dev
