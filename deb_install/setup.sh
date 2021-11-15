f06ba316aa4a2894ed0668ddb7ba7e34be82f08a
Adam McCartney <adam@mur.at>
2021-11-15 17:11:23 +0100

    Updated setup.sh to now be overly verbose and swanky

--- deb_install/setup.sh
+++ deb_install/setup.sh
@@ -1,9 +1,35 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-# This needs to be run with sudo
+# setup.sh: this is an initial setup script for a fresh debian install. The
+# script needs to be run with sudo priveledges and will try to install the
+# packages listed in pkglist.txt
 
-apt update
-apt upgrade -y
 
-apt-get install -y $(cat ./pkglist.txt)
+function installed {
+  cmd=$(command -v "${1}")
+
+  [[ -n "${cmd}" ]] && [[ -f "${cmd}" ]]
+  return ${?}
+}
+
+function die {
+  >&2 echo "Fatal: ${@}"
+  exit 1
+}
+
+
+[[ "${BASH_VERSINFO[0]}" -lt 4 ]] && die "Bash >=4 required"
+
+deps=(curl nc dig)
+for dep in "${deps[@]}"; do
+  installed "${dep}" || die "Missing '${dep}'"
+done
+
+
+do
+    apt update
+    apt upgrade -y
+
+    apt-get install -y $(cat ./pkglist.txt)
+done
 
