77207156015fd0843703047f484c7be0f07ce450
Adam McCartney <mccartney@mdw.ac.at>
2022-06-27 08:36:25 +0200

    Add full tangle of Emacs.org

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -12,20 +12,6 @@
                               (time-subtract after-init-time before-init-time)))
                      gcs-done)))
 
-;; init.el --- -*- lexical-binding: t; -*-
-
-;; The default is 800 kilobytes. Measured in bytes.
-(setq gc-cons-threshold (* 50 1000 1000))
-
-; Profile emacs startup
-(add-hook 'emacs-startup-hook
-          (lambda ()
-            (message "*** Emacs loaded in %s with %d garbage collections."
-                     (format "%.2f seconds"
-                             (float-time
-                              (time-subtract after-init-time before-init-time)))
-                     gcs-done)))
-
 ;; bootstrap straight.el
    (defvar bootstrap-version)
 (let ((bootstrap-file
@@ -104,3 +90,243 @@
   (global-hl-line-mode 1)
   (custom-set-faces
    '(cursor ((t :background "#eebb28")))))
+
+(straight-use-package '(dired :type built-in))
+(use-package dired
+  :config
+  (progn
+    (add-hook 'dired-mode-hook #'toggle-truncate-lines)
+    (setf dired-listing-switches "-alhG"
+          dired-guess-shell-alist-user
+          '(("\\.pdf\\'" "evince")
+            ("\\(\\.ods\\|\\.xlsx?\\|\\.docx?\\|\\.csv\\)\\'" "libreoffice")
+            ("\\(\\.png\\|\\.jpe?g\\)\\'" "qiv")
+            ("\\.gif\\'" "animate")))))
+
+(use-package evil
+  :init
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  (setq evil-want-C-u-scroll t)
+  (setq evil-want-C-i-jump nil)
+  :config
+  (evil-mode 1)
+  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
+  (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
+
+  ;; Use visual line motions even outside of visual-line-mode buffers
+  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
+  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
+
+  (evil-set-initial-state 'messages-buffer-mode 'normal)
+  (evil-set-initial-state 'dashboard-mode 'normal))
+
+(use-package evil-collection
+  :after evil
+  :config
+  (evil-collection-init))
+
+(use-package paren
+  :config (show-paren-mode))
+
+(use-package rainbow-delimiters
+  :defer t
+  :init
+  (add-hook 'emacs-lisp-mode-hook #'rainbow-delimiters-mode)
+  (add-hook 'ielm-mode-hook #'rainbow-delimiters-mode)
+  :config
+  (set-face-foreground 'rainbow-delimiters-depth-1-face "snow4")
+  (setf rainbow-delimiters-max-face-count 1)
+  (set-face-attribute 'rainbow-delimiters-unmatched-face nil
+                      :foreground 'unspecified
+                      :inherit 'error)
+  (set-face-foreground 'rainbow-delimiters-depth-1-face "snow4"))
+
+
+(use-package javadoc-lookup
+  :defer t
+  :bind ("C-h j" . javadoc-lookup)
+  :config
+  (ignore-errors
+    (setf javadoc-lookup-cache-dir (locate-user-emacs-file "local/javadoc"))))
+
+(use-package browse-url
+  :defer t
+  :init
+  (setf url-cache-directory (locate-user-emacs-file "local/url"))
+  :config
+  (when (executable-find "firefox")
+    (setf browse-url-browser-function #'browse-url-firefox)))
+
+(straight-use-package '(org :type build-in))
+;; Org mode
+(use-package org 
+  :init
+  (add-hook 'org-mode-hook '(lambda () (setq fill-column 72)))
+  (add-hook 'org-mode-hook 'turn-on-auto-fill)
+
+  (add-hook 'org-mode-hook
+            (lambda () (add-hook 'after-save-hook #'org-babel-tangle
+                                 :append :local)))
+  (setq org-todo-keywords
+        '((sequence "TODO" "IN-PROGRESS" "WAITING" "DONE")))
+
+  ;; org babel
+  (org-babel-do-load-languages
+   'org-babel-load-languages
+   '((emacs-lisp . t)
+     (python . t)
+     (shell . t)))
+
+  (push '("conf-unix" . conf-unix) org-src-lang-modes)
+  
+  ;; Capture templates
+  (setq org-capture-templates
+        '(("w" "Work Todo" entry (file+headline "~/Documents/org/Planner-mdw2022.org" "Tasks")
+           "* TODO %?\n %i\n %a")
+          ("h" "Home Todo" entry (file+headline "~/Documents/org/Planner-home2022.org" "Tasks")
+           "* TODO %?\n %i\n %a")))
+  (setq org-startup-folded "overview"))
+
+(require 'org-tempo)
+(add-to-list 'org-structure-template-alist '("sh" . "src shell"))
+(add-to-list 'org-structure-template-alist '("sql" . "src sql"))
+(add-to-list 'org-structure-template-alist '("yml" . "src yaml"))
+(add-to-list 'org-structure-template-alist '("el" . "src emacs-lisp"))
+(add-to-list 'org-structure-template-alist '("py" . "src python"))
+
+(use-package markdown-mode
+  :defer t
+  :mode ("\\.md$" "\\.markdown$" "vimperator-.+\\.tmp$")
+  :config
+  (add-hook 'markdown-mode-hook
+            (lambda ()
+              (remove-hook 'fill-nobreak-predicate
+                           'markdown-inside-link-p t)))
+  (setf sentence-end-double-space nil
+        markdown-indent-on-enter nil
+        markdown-command
+        "pandoc -f markdown -t html5 -s --self-contained --smart"))
+
+(use-package ivy
+  :diminish
+  :bind (("C-s" . swiper)
+         :map ivy-minibuffer-map
+         ("TAB" . ivy-alt-done)
+         ("C-l" . ivy-alt-done)
+         ("C-j" . ivy-next-line)
+         ("C-k" . ivy-previous-line)
+         :map ivy-switch-buffer-map
+         ("C-k" . ivy-previous-line)
+         ("C-l" . ivy-done)
+         ("C-d" . ivy-switch-buffer-kill)
+         :map ivy-reverse-i-search-map
+         ("C-k" . ivy-previous-line)
+         ("C-d" . ivy-reverse-i-search-kill))
+  :config
+  (ivy-mode 1))
+
+(use-package counsel
+  :bind (("M-x" . counsel-M-x)
+         ("C-x b" . counsel-ibuffer)
+         ("C-x C-f" . counsel-find-file)
+         :map minibuffer-local-map
+         ("C-r" . 'counsel-minibuffer-history)))
+
+(use-package lsp-mode
+  :commands (lsp lsp-deferred)
+  :hook (lsp-mode . efs/lsp-mode-setup)
+  :init
+  (setq lsp-keymap-prefix "C-c l")  ;; Or 'C-l', 's-l'
+  :config
+  (lsp-enable-which-key-integration t))
+
+(use-package lsp-ui
+  :hook (lsp-mode . lsp-ui-mode)
+  :custom
+  (lsp-ui-doc-position 'bottom))
+
+(use-package lsp-ivy)
+
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
+  :config
+ (setq which-key-idle-delay 1))
+
+(use-package projectile
+  :diminish projectile-mode
+  :config (projectile-mode)
+  :custom ((projectile-completion-system 'ivy))
+  :bind-keymap
+  ("C-c p" . projectile-command-map)
+  :init
+  ;; NOTE: Set this to the folder where you keep your Git repos!
+  (when (file-directory-p "~/.local/src")
+    (setq projectile-project-search-path '("~/.local/src" "~/Code" "~/.local/src/mdw")))
+  (setq projectile-switch-project-action #'projectile-dired))
+
+(use-package counsel-projectile
+  :config (counsel-projectile-mode))
+
+(use-package magit
+  :straight t
+  :custom
+  (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
+
+(defun efs/lsp-mode-setup ()
+  (setq lsp-headerline-breadcrumb-segments '(path-up-to-project file symbols))
+  (lsp-headerline-breadcrumb-mode))
+
+;;(use-package lsp-pyright
+;;  :straight t 
+;;  :hook (python-mode . (lambda ()
+;;                          (require 'lsp-pyright)
+;;                          (lsp)))) ;; or lsp-deferred
+
+(use-package slime
+  :init 
+  (setq inferior-lisp-program "/usr/local/bin/sbcl"))
+
+(use-package python-mode
+  :mode "\\.py\\'"
+  :hook (python-mode . lsp-deferred)
+  :config
+  (setq python-indent-level 4))
+
+(use-package typescript-mode
+  :mode "\\.ts\\'"
+  :hook (typescript-mode . lsp-deferred)
+  :config
+  (setq typescript-indent-level 2))
+  
+(defun dw/set-js-indentation ()
+  (setq js-indent-level 2)
+  (setq evil-shift-width js-indent-level)
+  (setq-default tab-width 2))
+
+(use-package js2-mode
+  :mode "\\.jsx?\\'"
+  :config
+  ;; Use js2-mode for Node scripts
+  (add-to-list 'magic-mode-alist '("#!/usr/bin/env node" . js2-mode))
+
+  ;; Don't use built-in syntax checking
+  (setq js2-mode-show-strict-warnings nil)
+
+  ;; Set up proper indentation in JavaScript and JSON files
+  (add-hook 'js2-mode-hook #'dw/set-js-indentation)
+  (add-hook 'json-mode-hook #'dw/set-js-indentation))
+
+(use-package geiser
+  :straight t
+  :config
+  (setq geiser-default-implementation 'guile)
+  (setq geiser-active-implementations '(guile))
+  (setq geiser-implementations-alist '(((regexp "\\.scm$") guile)))
+  (setq geiser-guile-binary "/usr/bin/guile"))
+
+(use-package geiser-guile
+  :straight t)
+
+(provide 'init) ; make (require 'init) happy
