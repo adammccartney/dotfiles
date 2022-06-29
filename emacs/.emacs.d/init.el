2729d09469c1d3c4b5e77fe98373ee29fe5564c2
Adam McCartney <mccartney@mdw.ac.at>
2022-06-29 09:53:09 +0200

    Add cleanup routines
    Ensures that transitory files are stored not in the git repository
    tracking the dotfiles repo.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -37,10 +37,28 @@
 
 ;; Clean up unused repos with `straight-remove-unused-repos'
 
-(push "~/.emacs.d/lisp" load-path)  
+;; Change the user-emacs-directory to keep unwanted things out of ~/.emacs.d
+(setq user-emacs-directory (expand-file-name "~/.cache/emacs/")
+      url-history-file (expand-file-name "url/history" user-emacs-directory))
 
+;; Use no-littering to automatically set common paths to the new user-emacs-directory
+(use-package no-littering
+  :straight t)
+
+;; Keep customization settings in a temporary file (thanks Ambrevar!)
+(setq custom-file
+      (if (boundp 'server-socket-dir)
+          (expand-file-name "custom.el" server-socket-dir)
+        (expand-file-name (format "emacs-custom-%s.el" (user-uid)) temporary-file-directory)))
+(load custom-file t)
+
+(push "~/.emacs.d/lisp" load-path)  
 (require 'unannoy)
 (require 'ad-mail)
+
+(set-default-coding-systems 'utf-8)
+
+(server-start)
 
 ;; Some global keybindings
 (global-set-key (kbd "C-x k") #'kill-this-buffer)
