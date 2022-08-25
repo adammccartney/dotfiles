909b1a3434d8af6c75a5bde510dea0a4a707612d
adammccartney <adam@mur.at>
2022-08-25 19:27:01 +0200

    Add support for gnuplot
    Important plotting devices. Squiggly lines ahoy!

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -268,6 +268,7 @@
      (python . t)
      (scheme . t)
      (go . t)
+     (gnuplot . t)
      (shell . t)))
 
   (push '("conf-unix" . conf-unix) org-src-lang-modes)
@@ -291,6 +292,7 @@
 (add-to-list 'org-structure-template-alist '("yml" . "src yaml"))
 (add-to-list 'org-structure-template-alist '("go" . "src go"))
 (add-to-list 'org-structure-template-alist '("el" . "src emacs-lisp"))
+(add-to-list 'org-structure-template-alist '("gnpl" . "src gnuplot"))
 (add-to-list 'org-structure-template-alist '("py" . "src python"))
 
 (defun my/org-roam-node-list ()
@@ -648,6 +650,9 @@ company-yasnippet' to all company backends."
 ;;  :hook (python-mode . (lambda ()
 ;;                          (require 'lsp-pyright)
 ;;                          (lsp)))) ;; or lsp-deferred
+
+(use-package gnuplot-mode)
+(use-package gnuplot)
 
 (use-package web-mode
   :mode "(\\.\\(html?\\|ejs\\|tsx|jsx\\)\\'"
