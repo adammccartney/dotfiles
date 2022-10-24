40ad90e239cd19aa410b589e9dfb2215aae791b4
adammccartney <adam@mur.at>
2022-10-24 06:58:24 +0200

    Add support for the modus themes
    Also a small elisp function for retrieving rfc memos.

--- emacs/.emacs.d/lisp/adlisp.el
+++ emacs/.emacs.d/lisp/adlisp.el
@@ -1,9 +1,16 @@
 ;; Some custom elisp functions
 
 (defun ad/buffer-insert-sh-output (cmd)
- "Run a shell command and insert the output to the current buffer at point"
+ "Run a shell command and insert the output to the current buffer at point."
 
  (interactive)
  (insert (format "%s" (shell-command-to-string cmd))))
 
+(defun ad/get-rfc (rfcnum)
+  "Get an pull an rfc via http. Plonk the contents into buffer at point."
+  (interactive)
+  (insert (format "%s" (shell-command-to-string (format "curl https://www.rfc-editor.org/rfc/rfc%s.txt" rfcnum)))))
+
 (provide 'adlisp)
+
+
