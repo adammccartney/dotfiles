a61de9eb44d83166dc4a2d9e3e9ccc48eec8eaf4
adammccartney <adam@mur.at>
2022-10-24 21:43:21 +0200

    Giving the lsp-mode stuff a rest for a while in favor of eglot.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -84,6 +84,125 @@
 (add-to-list 'auto-mode-alist '("\\.mak$" . makefile-gmake-mode))
 (add-to-list 'auto-mode-alist '("\\.make$" . makefile-gmake-mode))
 
+(use-package modus-themes
+  :ensure t
+  :config
+  (load-theme 'modus-vivendi)
+  ;; Main typeface
+  (set-face-attribute 'default nil :family "DejaVu Sans Mono" :height 110)
+  ;; Proportionally spaced typeface
+  (set-face-attribute 'variable-pitch nil :family "DejaVu Serif" :height 1.0)
+  ;; Monospaced typeface
+  (set-face-attribute 'fixed-pitch nil :family "DejaVu Sans Mono" :height 1.0)
+  (setq modus-themes-italic-constructs t
+        modus-themes-bold-constructs nil
+        modus-themes-mixed-fonts nil
+        modus-themes-subtle-line-numbers t
+        modus-themes-intense-mouseovers nil
+        modus-themes-deuteranopia t
+        modus-themes-tabs-accented t
+        modus-themes-variable-pitch-ui nil
+        modus-themes-inhibit-reload t ; only applies to `customize-set-variable' and related
+
+        modus-themes-fringes nil ; {nil,'subtle,'intense}
+
+        ;; Options for `modus-themes-lang-checkers' are either nil (the
+        ;; default), or a list of properties that may include any of those
+        ;; symbols: `straight-underline', `text-also', `background',
+        ;; `intense' OR `faint'.
+        modus-themes-lang-checkers nil
+
+        ;; Options for `modus-themes-mode-line' are either nil, or a list
+        ;; that can combine any of `3d' OR `moody', `borderless',
+        ;; `accented', a natural number for extra padding (or a cons cell
+        ;; of padding and NATNUM), and a floating point for the height of
+        ;; the text relative to the base font size (or a cons cell of
+        ;; height and FLOAT)
+        modus-themes-mode-line '(3d accented borderless (padding . 4) (height . 0.9))
+
+        ;; Same as above:
+        ;; modus-themes-mode-line '(accented borderless 4 0.9)
+
+        ;; Options for `modus-themes-markup' are either nil, or a list
+        ;; that can combine any of `bold', `italic', `background',
+        ;; `intense'.
+        modus-themes-markup '(background italic)
+
+        ;; Options for `modus-themes-syntax' are either nil (the default),
+        ;; or a list of properties that may include any of those symbols:
+        ;; `faint', `yellow-comments', `green-strings', `alt-syntax'
+        modus-themes-syntax nil
+
+        ;; Options for `modus-themes-hl-line' are either nil (the default),
+        ;; or a list of properties that may include any of those symbols:
+        ;; `accented', `underline', `intense'
+        modus-themes-hl-line '(underline accented)
+
+        ;; Options for `modus-themes-paren-match' are either nil (the
+        ;; default), or a list of properties that may include any of those
+        ;; symbols: `bold', `intense', `underline'
+        modus-themes-paren-match '(bold intense)
+
+        ;; Options for `modus-themes-links' are either nil (the default),
+        ;; or a list of properties that may include any of those symbols:
+        ;; `neutral-underline' OR `no-underline', `faint' OR `no-color',
+        ;; `bold', `italic', `background'
+        modus-themes-links '(neutral-underline background)
+
+        ;; Options for `modus-themes-box-buttons' are either nil (the
+        ;; default), or a list that can combine any of `flat', `accented',
+        ;; `faint', `variable-pitch', `underline', `all-buttons', the
+        ;; symbol of any font weight as listed in `modus-themes-weights',
+        ;; and a floating point number (e.g. 0.9) for the height of the
+        ;; button's text.
+        modus-themes-box-buttons '(variable-pitch flat faint 0.9)
+
+        ;; Options for `modus-themes-prompts' are either nil (the
+        ;; default), or a list of properties that may include any of those
+        ;; symbols: `background', `bold', `gray', `intense', `italic'
+        modus-themes-prompts '(intense bold)
+
+        ;; The `modus-themes-completions' is an alist that reads three
+        ;; keys: `matches', `selection', `popup'.  Each accepts a nil
+        ;; value (or empty list) or a list of properties that can include
+        ;; any of the following (for WEIGHT read further below):
+        ;;
+        ;; `matches' - `background', `intense', `underline', `italic', WEIGHT
+        ;; `selection' - `accented', `intense', `underline', `italic', `text-also' WEIGHT
+        ;; `popup' - same as `selected'
+        ;; `t' - applies to any key not explicitly referenced (check docs)
+        ;;
+        ;; WEIGHT is a symbol such as `semibold', `light', or anything
+        ;; covered in `modus-themes-weights'.  Bold is used in the absence
+        ;; of an explicit WEIGHT.
+        modus-themes-completions '((matches . (extrabold))
+                                   (selection . (semibold accented))
+                                   (popup . (accented intense)))
+
+        modus-themes-mail-citations nil ; {nil,'intense,'faint,'monochrome}
+
+        ;; Options for `modus-themes-region' are either nil (the default),
+        ;; or a list of properties that may include any of those symbols:
+        ;; `no-extend', `bg-only', `accented'
+        modus-themes-region '(bg-only no-extend)
+
+        ;; Options for `modus-themes-diffs': nil, 'desaturated, 'bg-only
+        modus-themes-diffs 'desaturated
+
+        modus-themes-org-blocks 'gray-background ; {nil,'gray-background,'tinted-background}
+
+        modus-themes-org-agenda ; this is an alist: read the manual or its doc string
+        '((header-block . (variable-pitch 1.3))
+          (header-date . (grayscale workaholic bold-today 1.1))
+          (event . (accented varied))
+          (scheduled . uniform)
+          (habit . traffic-light))
+
+        modus-themes-headings ; this is an alist: read the manual or its doc string
+        '((1 . (overline background variable-pitch 1.3))
+          (2 . (rainbow overline 1.1))
+          (t . (semibold)))))
+
 (use-package spacegray-theme
   :defer t)
 
@@ -93,7 +212,7 @@
   ;; Global settings (defaults)
   (setq doom-themes-enable-bold t
         doom-themes-enable-italic t)
-  (load-theme 'doom-nord t)
+  ;;(load-theme 'doom-nord t)
 
   ;; Enable flashing mode-line on errors
   (doom-themes-visual-bell-config)
@@ -116,6 +235,7 @@
   :hook (doom-modeline-mode . minions-mode))
 
 (use-package doom-modeline
+  :ensure t
   :after eshell
   :hook (after-init . doom-modeline-init)
   :custom-face
@@ -590,24 +710,6 @@ company-yasnippet' to all company backends."
 
   (fk/company-enable-snippets))
 
-(use-package dap-mode
-  :ensure t
-  :after (lsp-mode)
-  :functions dap-hydra/nil
-  :bind (:map lsp-mode-map
-         ("<f5>" . dap-debug)
-         ("M-<f5>" . dap-hydra))
-  :hook ((dap-mode . dap-ui-mode)
-    (dap-session-created . (lambda (&_rest) (dap-hydra)))
-    (dap-terminated . (lambda (&_rest) (dap-hydra/nil)))))
-
-(use-package lsp-treemacs
-  :after (lsp-mode treemacs)
-  :ensure t
-  :commands lsp-treemacs-errors-list
-  :bind (:map lsp-mode-map
-         ("M-9" . lsp-treemacs-errors-list)))
-
 (use-package treemacs
   :ensure t
   :commands (treemacs)
@@ -617,39 +719,14 @@ company-yasnippet' to all company backends."
 
 (use-package treemacs-projectile)
 
-(use-package lsp-mode
-  :init 
-  (setq lsp-keymap-prefix "C-c l")
-  :hook ((python-mode . lsp)
-         ((typescript-mode js2-mode web-mode) . lsp)
-         (docker-mode . lsp)
-         (lsp-mode . lsp-enable-which-key-integration))
-  :commands lsp)
-
-(use-package lsp-ui
-  :after lsp
-  :hook (lsp-mode . lsp-ui-mode)
-  :config
-  (setq lsp-ui-sideline-enable t)
-  (lsp-ui-doc-show))
-
-(use-package lsp-ivy)
-
 (use-package which-key
   :init (which-key-mode)
   :diminish which-key-mode
   :config
  (setq which-key-idle-delay 1))
 
-(use-package lsp-pyright
-  :after lsp-mode
-  :custom
-  (lsp-pyright-auto-import-completions nil)
-  (lsp-pyright-typechecking-mode "off")
-  :ensure t
-  :hook (python-mode . (lambda ()
-                         (require 'lsp-pyright)
-                         (lsp))))
+(use-package eglot
+  :ensure t)
 
 (use-package projectile
   :diminish projectile-mode
