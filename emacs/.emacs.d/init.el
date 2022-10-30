5661d97613ed2729566fc3f647f0d1384be545ae
adammccartney <adam@mur.at>
2022-10-30 21:59:51 +0100

    Add hydra
    This is quite a cool package.
    See the github: https://github.com/abo-abo/hydra

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -598,6 +598,35 @@ GROUP BY id")))
 (add-hook 'xref-backend-functions #'dumb-jump-xref-activate)
 (setq xref-show-definitions-function #'xref-show-definitions-completing-read)
 
+(use-package hydra
+  :ensure t)
+
+(global-set-key
+ (kbd "C-M-o")
+  (defhydra hydra-window ()
+    "window"
+    ("h" windmove-left)
+    ("j" windmove-down)
+    ("k" windmove-up)
+    ("l" windmove-right)
+    ("v" (lambda ()
+      (interactive)
+      (split-window-right)
+      (windmove-right))
+      "vert")
+    ("x" (lambda ()
+           (interactive)
+           (split-window-below)
+           (windmove-down))
+       "horz")
+    ("o" delete-other-windows "one" :color blue)
+    ("a" ace-window "ace")
+    ("s" ace-swap "swap")
+    ("d" ace-delete-window "ace-one" :color blue)
+    ("b" ido-switch-buffer "buf")
+    ("m" headlong-bookmark "buf")
+    ("q" nil "cancel")))
+
 (use-package treemacs
   :ensure t
   :commands (treemacs)
