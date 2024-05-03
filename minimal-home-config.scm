40bbf1571ab8e21ecde6aa4c02f99d246fe46935
Adam McCartney <adam.mccartney@tuwien.ac.at>
2024-05-03 16:41:58 +0200

    Ripgrep tools

--- minimal-home-config.scm
+++ minimal-home-config.scm
@@ -19,6 +19,8 @@
                                            "emacs-no-x-toolkit"  ; TODO figure out why regular emacs borks the desktop cursor
                                            "emacs-geiser"
                                            "emacs-yasnippet"
+                                           "emacs-rg"
+                                           "ripgrep"
                                            "git"
                                            "fzf"
                                            "vim"
