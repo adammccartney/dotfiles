820ff2ce2293ba37bb6aaf46a441dc23da18771d
Adam McCartney <adam.mccartney@tuwien.ac.at>
2024-05-14 16:53:45 +0200

    Add emacs manifest

---
+++ guix/manifests/emacs.scm
@@ -0,0 +1,8 @@
+(specifications->manifest
+ ;; Emacs
+ '("emacs-no-x-toolkit"  ; TODO figure out why regular emacs borks the desktop cursor
+   "emacs-geiser"
+   "emacs-yasnippet"
+   "emacs-rg"
+   "libvterm"
+   "libtool"))
