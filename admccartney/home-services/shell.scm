5d7da36ac8059cf1e47e11e75d1a738ef7f2dd97
Adam McCartney <adam@mur.at>
2024-05-19 22:39:29 +0200

    Comment out profile config

--- admccartney/home-services/shell.scm
+++ admccartney/home-services/shell.scm
@@ -42,6 +42,7 @@ export HISTFILE=$XDG_CACHE_HOME/.bash_history")))
                ("MAILDIR" . "$HOME/.mail")))
             
             (bashrc
+<<<<<<< HEAD
              `(,(local-file "../../files/bash-prompt")
                ;;,(local-file "../../files/bash-profile")
                ,(local-file "../../files/bash-functions")
