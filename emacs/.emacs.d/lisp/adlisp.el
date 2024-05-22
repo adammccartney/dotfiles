9fecf3c9610031ddcbb2052f974801366ae3d272
Adam McCartney <adam@mur.at>
2024-05-22 16:13:19 +0200

    Add a fedora complient systemd unit file for emacs

--- emacs/.emacs.d/lisp/adlisp.el
+++ emacs/.emacs.d/lisp/adlisp.el
@@ -14,3 +14,28 @@
 (provide 'adlisp)
 
 
+;; Conversion functions
+
+(defun elisp->json-list-str (lst)
+  ;; Convert an elisp list to a json formatted list
+  ;; i.e. string output as
+  ;; [item1, item2, ..., itemn]
+
+  (defun iter-elisp->json-list-str (lst res)
+    ;; Iterate until end of lst, then return res
+    (if (null lst)
+        (progn
+          res)
+      (progn
+        (if (= 1 (length lst))
+            (setq res (format "%s\"%s\"]" res (car lst)))
+          (setq res (format "%s\"%s\"," res (car lst))))
+        (iter-elisp->json-list-str (cdr lst) res))))
+  (iter-elisp->json-list-str lst "["))
+
+
+(defun test-elisp->json-list-str ()
+  (setq tlst (list "this"))
+  (setq lst (list "Network Boot" "UEFI IPv4: Network 00 at Riser 01 Slot 02" "UEFI HTTPv6: Network 00 at Riser 01 Slot 02" "UEFI HTTPv4: Network 00 at Riser 01 Slot 02" "UEFI HTTPv6: Network 00 at Riser 02 Slot 02" "UEFI HTTPv4: Network 00 at Riser 02 Slot 02" "UEFI IPv4: Network 00 at Riser 02 Slot 02" "UEFI IPv6: Network 00 at Riser 02 Slot 02" "UEFI HTTPv6: Network 01 at Riser 02 Slot 02" "UEFI HTTPv4: Network 01 at Riser 02 Slot 02" "UEFI IPv4: Network 01 at Riser 02 Slot 02" "UEFI IPv6: Network 01 at Riser 02 Slot 02" "UEFI HTTPv6: Intel I210 Network 00 at Baseboard" "UEFI HTTPv4: Intel I210 Network 00 at Baseboard" "UEFI IPv4: Intel I210 Network 00 at Baseboard" "UEFI IPv6: Intel I210 Network 00 at Baseboard" "Launch EFI Shell" "Enter Setup" "Boot Device List"))
+  (setq res "[\"Network Boot\",\"UEFI IPv4: Network 00 at Riser 01 Slot 02\",\"UEFI HTTPv6: Network 00 at Riser 01 Slot 02\",\"UEFI HTTPv4: Network 00 at Riser 01 Slot 02\",\"UEFI HTTPv6: Network 00 at Riser 02 Slot 02\",\"UEFI HTTPv4: Network 00 at Riser 02 Slot 02\",\"UEFI IPv4: Network 00 at Riser 02 Slot 02\",\"UEFI IPv6: Network 00 at Riser 02 Slot 02\",\"UEFI HTTPv6: Network 01 at Riser 02 Slot 02\",\"UEFI HTTPv4: Network 01 at Riser 02 Slot 02\",\"UEFI IPv4: Network 01 at Riser 02 Slot 02\",\"UEFI IPv6: Network 01 at Riser 02 Slot 02\",\"UEFI HTTPv6: Intel I210 Network 00 at Baseboard\",\"UEFI HTTPv4: Intel I210 Network 00 at Baseboard\",\"UEFI IPv4: Intel I210 Network 00 at Baseboard\",\"UEFI IPv6: Intel I210 Network 00 at Baseboard\",\"Launch EFI Shell\",\"Enter Setup\",\"Boot Device List\"]")
+  (elisp->json-list-str lst))
